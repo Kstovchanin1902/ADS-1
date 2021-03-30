3ed7f3bccb80e4a5cd9e380ca12ecb902fe51b1e
Kstovchanin1902 <78904695+Kstovchanin1902@users.noreply.github.com>
2021-03-30 07:18:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,35 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int i = size/2, j, count = 0;
+    while(i > 1 and i < (size-1)){
+        j = i;
+        cout<<"i = "<<i<<endl;
+        if (arr[i] == value) {
+            count++;
+            cout<<"i = "<< i << endl;
+            while (arr[i] == value){
+                i--;
+                if (arr[i] == value){
+                    count++;
+                    cout<<"i = "<<i<<", count = "<<count<<endl;
+                }
+            }
+            while (arr[j] == value){
+                j++;
+                if (arr[j] == value){
+                    count++;
+                    cout<<"j = "<<j<<", count = "<<count<<endl;
+                }
+            }
+            return count;
+        } 
+        else if (arr[i]<value) {
+                 i = i + (size-i)/2;
+            } else if (arr[i]>value) {
+                        i = i - (size-i)/2;
+                    }
+        }
+        return 0;
   return 0; // если ничего не найдено
 }
