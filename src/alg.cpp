e4cd7f009abc4265b379f295482f31ac0fb1a41d
Kstovchanin1902 <78904695+Kstovchanin1902@users.noreply.github.com>
2021-03-30 07:24:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,7 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
+  //  поместить сюда реализацию алгоритма
   int i = size/2, j, count = 0;
     while(i > 1 and i < (size-1)){
         j = i;
