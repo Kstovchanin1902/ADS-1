08438dc42a522cdeb1f8d63c96250dc082942837
Kstovchanin1902 <78904695+Kstovchanin1902@users.noreply.github.com>
2021-03-30 08:07:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,31 +2,34 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
-  int i = size/2, j, count = 0;
-  while (i > 1 && i < (size-1)) {
+  int j, count = 0, left=0, right = size;
+     do {
+        int i = (left + right)/2;
         j = i;
+        cout<<"i = "<<i<<endl;
         if (arr[i] == value) {
             count++;
+            cout<<"i = "<< i << endl;
             while (arr[i] == value) {
                 i--;
                 if (arr[i] == value) {
                     count++;
+                    cout<<"i = "<<i<<", count = "<<count<<endl;
                 }
             }
             while (arr[j] == value) {
                 j++;
                 if (arr[j] == value) {
                     count++;
+                    cout<<"j = "<<j<<", count = "<<count<<endl;
                 }
             }
             return count;
-        } 
-        else if (arr[i] < value) {
-                 i = i + (size-i)/2;
+        } else if (arr[i] < value) {
+                left = i+1;
             } else if (arr[i] > value) {
-                        i = i - (size - i)/2;
+                        right = i - 1;
                     }
-        }
-        return 0;
-  return 0; // если ничего не найдено
+        }  while (left <= right);  
+     return 0; // если ничего не найдено
 }
